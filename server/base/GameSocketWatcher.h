882a936cce1393f7487cb0be03957737f1923d1a
MacroGu <macrogu@qq.com>
2019-12-04 00:16:22 +0800

    增加一些接口

--- server/base/GameSocketWatcher.h
+++ server/base/GameSocketWatcher.h
@@ -24,17 +24,26 @@ struct stMsgToClient
 class GameSocketWatcher : public BaseSocketWatcher
 {
 public:
+	// 一些暴露给游戏逻辑的接口
+	// 广播 1 ， 由客户端收到消息后，经过处理，再进行广播，
+
+	// 广播 2 ，服务器产生的事件，比如定时事件，而主动广播
+
+
+	// 发送给指定客户端
+
+public:
 	GameSocketWatcher();
 
 	virtual ~GameSocketWatcher();
 
-	virtual int OnEpollAcceptEvent(stSocketContext& socket_context);
+	virtual int OnEpollAcceptEvent(stSocketContext& socket_context) override;
 
-	virtual int OnEpollReadableEvent(int& epollfd, epoll_event& event);
+	virtual int OnEpollReadableEvent(int& epollfd, epoll_event& event) override;
 
-	virtual int OnEpollWriteableEvent(stSocketContext& socket_context);
+	virtual int OnEpollWriteableEvent(stSocketContext& socket_context) override;
 
-	virtual int OnEpollCloseEvent(stSocketContext& socket_context);
+	virtual int OnEpollCloseEvent(stSocketContext& socket_context) override;
 
 private:
 	// handle client message
